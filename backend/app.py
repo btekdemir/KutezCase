2c97896087e683d787d02ca112352af42774c57c
baris <baristekdemir89@gmail.com>
2024-11-21 18:55:45 +0300

    gold_price file is deleted

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,44 @@
 from fastapi import FastAPI
 import json
-from gold_price import get_gold_price
+#from gold_price import get_gold_price
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+import requests
+
+
+def get_gold_price():
+    """
+    Fetch the real-time gold price in USD per ounce from GoldAPI.
+    """
+    api_key = "goldapi-5k0oa19m3qf297w-io"
+    symbol = "XAU"
+    curr = "USD"
+    url = f"https://www.goldapi.io/api/{symbol}/{curr}"
+
+    headers = {
+        "x-access-token": api_key,
+        "Content-Type": "application/json",
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Raise exception for HTTP errors
+
+        # Parse the API response
+        data = response.json()
+        gold_price_per_ounce = data.get("price")
+
+        if gold_price_per_ounce is not None:
+            # Convert from ounce to gram
+            gold_price_per_gram = gold_price_per_ounce / 31.1035
+            return round(gold_price_per_gram, 2)
+        else:
+            print("Error: Gold price not found in the API response.")
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Error:", str(e))
+        return None
+
 
 
 
