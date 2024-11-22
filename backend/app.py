7cc7b09a1dc6b6b5d8e9cbf7c73c1888c100d286
btekdemir <139576092+btekdemir@users.noreply.github.com>
2024-11-22 22:19:33 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -49,9 +49,9 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello from FastAPI"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+#if __name__ == "__main__":
+    #import uvicorn
+    #uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
 # Add CORS middleware
 app.add_middleware(
